a60666430084fbecfe225edd21df4420fcfde284
aNTb-j <comercializacion.joaquin.lopez@gmail.com>
2026-07-30 10:51:43 -0300

    #

--- api/contact.js
+++ api/contact.js
@@ -4,72 +4,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
 
-    
     console.log("La función se ejecutó");
 
-        return res.status(200).json({
-            success: true,
-            message: "API funcionando"
-        });
-
-
-    // Log the API key to verify it's being read correctly
-    console.log("Resend API Key:", process.env.RESEND_API_KEY);
-    if (req.method !== "POST") {
-        return res.status(405).json({
-            message: "Method Not Allowed"
-        });
-    }
-    console.log("Request body:", req.body); // Log the request body to verify the data being sent
-    try {
-
-        const {
-            name,
-            surname,
-            company,
-            cuit,
-            email,
-            phone,
-            description
-        } = req.body;
-
-        await resend.emails.send({
-
-            from: "Formulario <onboarding@resend.dev>",
-
-            to: "comercializacion.joaquin.lopez@gmail.com",
-
-            subject: "Nuevo contacto",
-
-            html: `
-                <h2>Nuevo contacto</h2>
-
-                <p><b>Nombre:</b> ${name}</p>
-                <p><b>Apellido:</b> ${surname}</p>
-                <p><b>Empresa:</b> ${company}</p>
-                <p><b>CUIT:</b> ${cuit}</p>
-                <p><b>Email:</b> ${email}</p>
-                <p><b>Teléfono:</b> ${phone}</p>
-
-                <hr>
-
-                <p>${description}</p>
-            `
-        });
-
-        return res.status(200).json({
-            success: true
-        });
-
-    } catch (error) {
-
-        console.error(error);
-
-        return res.status(500).json({
-            success: false,
-            error: error.message
-        });
-
-    }
+    return res.status(200).json({
+        success: true,
+        message: "API funcionando"
+    });
 
 }
